6ccdb9a2db2b19d9b7d7aa0c7fb27e7a6ea83c48
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2020-09-15 15:29:41 -0400

    Premiere version OK

--- Lab0_210/ContentView.swift
+++ Lab0_210/ContentView.swift
@@ -16,91 +16,126 @@ struct ContentView: View {
     
     @State var dataString: String
     @State var name: String
+    @State var resultats: String
     var body: some View {
-      
+        
         VStack{
             
+            Image("ets")
+                .resizable()
+                .frame(width: 200, height: 200)
+                .cornerRadius(12)
             HStack{
                 Text("JeuDeDés-Node-Express-TS-TDD-LOG210")
+                    .font(.title)
+                    
             }
             
-         
-    
+            
+            
             TextField("Nom du nouveau joueur", text: $name)
                 
-                        .padding()
-                
-                
+                .padding()
+            
+            
             
             Button(action: {
                 self.demarrer(name: self.name)
             }){
                 Text("Démarrer")
+                  .padding(.vertical, 10)
+                                            .padding(.horizontal, 30)
+                                            .foregroundColor(.white)
+                                                      .background(Color.gray)
+                                                      .clipShape(Capsule())
+
             }
-            .padding()
+         
+       
             Button(action: {
                 self.redemarrerJeu()
             }){
                 Text("Redémarrer")
+                 .padding(.vertical, 10)
+                              .padding(.horizontal, 30)
+                              .foregroundColor(.white)
+                                        .background(Color.gray)
+                                        .clipShape(Capsule())
+                    
+            }
+               .padding()
+          
+            
+            Button(action: {
+                self.joueur(name: self.name)
+            }){
+                Text("Jouer!")
+                .padding(.vertical, 10)
+                .padding(.horizontal, 30)
+                .foregroundColor(.white)
+                          .background(Color.gray)
+                          .clipShape(Capsule())
             }
             .padding()
             
-            Text("Joueurs")
-            .bold()
             
-            HStack (){
+            Text("Joueur")
+                .bold()
+            
+            VStack (){
                 
                 Text("\(name)")
+                Text("Résultats et Debug")
+                    .bold()
+                    .padding()
+                Text("\(self.resultats)")
             }
         }
     }
-    //test post with alamoFire
-    fileprivate func uploadDocument(name:String) {
-        let headers: HTTPHeaders = [
-            "Content-type": "multipart/form-data"
-        ]
-
-        AF.request( urlDemarrerJeu, method: HTTPMethod(rawValue: urlRedemarrerJeu))
-        .response { request in
-                     print(request)
-                    
-                 }
-        AF.upload(
-            multipartFormData: { multipartFormData in
-                multipartFormData.append("test".data(using: String.Encoding.utf8)!, withName: "nom")
-        },
-            to: urlRedemarrerJeu, method: .post , headers: headers)
-         
-            .response { response in
-                if let data = response.data{
-                    //handle the response however you like
-                    let responseString = String(data: data, encoding: .utf8)
-                                print("responseString = \(responseString)")
-                    
-                    
-                    
-                }
-                
-
+    func joueur (name: String) {
+        guard let url  = URL(string: "http://localhost:3000/api/v1/jeu/jouer/\(name)") else {return}
+        var request = URLRequest(url: url)
+        request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
+        request.setValue("Cache-Control", forHTTPHeaderField: "no-cache")
+        
+        request.setValue("/api/v1/jeu/redemarrerJeu", forHTTPHeaderField: "Accept")
+        request.setValue("/api/v1/jeu/redemarrerJeu", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("error=\(error)")
+                return
+            }
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                self.resultats =  String(data: data, encoding: .utf8)!
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            self.resultats =  String(data: data, encoding: .utf8)!
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
         }
+        task.resume()
     }
+    
     //aide :
     //https://stackoverflow.com/questions/26364914/http-request-in-swift-with-post-method
     func demarrer (name: String) {
         
-         guard let url  = URL(string: urlDemarrerJeu) else {return}
-         var request = URLRequest(url: url)
-      var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
-
+        guard let url  = URL(string: urlDemarrerJeu) else {return}
+        var request = URLRequest(url: url)
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        
         //permet de POST avec le bon parametre
         components.queryItems = [
             //@State
             URLQueryItem(name: "nom", value: name)
         ]
- request.httpMethod = "POST"
+        request.httpMethod = "POST"
         let query = components.url!.query
-
-     
+        
+        
         request.httpBody = Data(query!.utf8)
         
         //POS la requete avec le bon parametres (name="nom")
@@ -109,50 +144,55 @@ struct ContentView: View {
                 print("error=\(error)")
                 return
             }
-            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                // check for http errors
+                self.resultats =  String(data: data, encoding: .utf8)!
                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
                 print("response = \(response)")
             }
             let responseString = String(data: data, encoding: .utf8)
+            self.resultats =  String(data: data, encoding: .utf8)!
             print("responseString = \(responseString)")
         }
         task.resume()
     }
     func redemarrerJeu (){
         
-         guard let url  = URL(string: urlRedemarrerJeu) else {return}
-         var request = URLRequest(url: url)
+        guard let url  = URL(string: urlRedemarrerJeu) else {return}
+        var request = URLRequest(url: url)
         request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
         request.setValue("Cache-Control", forHTTPHeaderField: "no-cache")
         
         request.setValue("/api/v1/jeu/redemarrerJeu", forHTTPHeaderField: "Accept")
-         request.setValue("/api/v1/jeu/redemarrerJeu", forHTTPHeaderField: "Content-Type")
-         request.httpMethod = "GET"
-   
-         let task = URLSession.shared.dataTask(with: request) { data, response, error in
-             guard let data = data, error == nil else {
-                 print("error=\(error)")
-                 return
-             }
-             if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
-                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                 print("response = \(response)")
-             }
-             let responseString = String(data: data, encoding: .utf8)
-             print("responseString = \(responseString)")
-         }
-         task.resume()
-    }
- 
-
+        request.setValue("/api/v1/jeu/redemarrerJeu", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
         
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("error=\(error)")
+                return
+            }
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                self.resultats =  String(data: data, encoding: .utf8)!
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            let responseString = String(data: data, encoding: .utf8)
+            self.resultats =  String(data: data, encoding: .utf8)!
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+    }
+    
+    
+    
     
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(dataString: "", name: "")
+        ContentView(dataString: "", name: "", resultats: "")
     }
 }
 
